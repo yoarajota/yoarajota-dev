3ed914735343a44bc8624562e2a2c18025cad010
yoarajota <joaovbsberse@gmail.com>
2022-07-31 22:27:44 -0300

    👌

--- public/components/nodes/nodeHead.js
+++ public/components/nodes/nodeHead.js
@@ -12,10 +12,12 @@ function NodeHead({ img, lvl, xp, children }) {
       minHeight="220px"
       textAlign="center"
     >
-      <Text m="0" p="5px" fontFamily="Ubuntu" fontSize="24px" color="#D99E6A">
+      <Text m="0" p="5px" fontFamily="Ubuntu" fontSize="26px" color="#D99E6A">
         {children[0]}
       </Text>
+      <Box m='10px 0'>
         {children[1]}
+      </Box>
       <Levelers xp={xp ? xp : ""} lvl={lvl ? lvl : ""} />
     </Box>
   );
--- public/images/reactsvg.js
+++ public/images/reactsvg.js
@@ -1,7 +1,7 @@
 export default function ReactSvg() {
   return (
     <svg
-      // width="50%"
+      width="40%"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="-11.5 -10.23174 23 20.46348"
     >
