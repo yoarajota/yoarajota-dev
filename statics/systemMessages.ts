91a6107420018b7fde7fe8ac6833b55c62355985
João Vítor Sberse <81939995+yoarajota@users.noreply.github.com>
2023-02-16 19:11:44 -0300

    messages

--- statics/systemMessages.ts
+++ statics/systemMessages.ts
@@ -2,12 +2,17 @@ import { keyable } from "asset/types";
 
 const Messages: keyable = {
   "pt-BR": {
-    resume_text:
-      "Desenvolvedor Full Stack proficiente em tecnologias de front-end e server-side.</br></br>Atualmente trazendo inovação, inteligência e alta performance no ramo de ERP com um sistema web complexo.</br></br>Ótimo membro de equipe, tutorando e dividindo conhecimento técnico com os envolvidos.</br></br>  Entenda um pouco melhor de mim;",
-  },
+    resume_text: "Desenvolvedor Full Stack proficiente em tecnologias de front-end e server-side.</br></br>Atualmente trazendo inovação, inteligência e alta performance no ramo de ERP com um sistema web complexo.</br></br>Ótimo membro de equipe, tutorando e dividindo conhecimento técnico com os envolvidos.</br></br>  Entenda um pouco melhor de mim;",
+    tech_title: "Tecnologias",
+    exp_title: "Experiências",
+    academy_title: "Vida Acadêmica",
+      
+},
   "en-US": {
-    resume_text:
-      "Fullstack Developer profficient in frot-end and server-side technologies.</br></br>At the moment bringing inovation, inteligence and high performance on ERP area with a complex web system.</br></br>Great team member, tutoring and sharing technical knowledge with the others.</br> </br> Know me better;",
+    resume_text: "Fullstack Developer profficient in frot-end and server-side technologies.</br></br>At the moment bringing inovation, inteligence and high performance on ERP area with a complex web system.</br></br>Great team member, tutoring and sharing technical knowledge with the others.</br> </br> Know me better;",
+    tech_title: "Technologies",
+    exp_title: "Experiences",
+    academy_title: "Academic Background",  
   },
 };
 
