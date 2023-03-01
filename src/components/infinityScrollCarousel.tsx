7412a3ac79e6e357fdf9a2b7356301118bf13cba
yoarajota <joaovbscontato@gmail.com>
2023-03-01 00:05:42 -0300

    start changes scroll

--- src/components/infinityScrollCarousel.tsx
+++ src/components/infinityScrollCarousel.tsx
@@ -2,34 +2,38 @@ import { Box, List, ListItem } from "@chakra-ui/react";
 import { Carousel, keyable } from "asset/types";
 import { useScroll } from "framer-motion";
 import _ from "lodash";
-import React, { ReactNode, SetStateAction, useEffect, useReducer, useRef, useState } from "react";
+import React, {
+  ReactNode,
+  SetStateAction,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
 
-const USER = 1
-const SYSTEM  = 2
+const USER = 1;
+const SYSTEM = 2;
 
 function InfinityScrollCarousel({ list }: Carousel) {
-    const [scroll, dispatch] = useReducer(
-      (prev: keyable, next: keyable) => ({ ...prev, ...next }),
-      { scroll_number: 0, call: USER }
-    );
-    const [state, setState] = useState<Array<string | ReactNode>>()
-    const [scrollNumber, setScrollNumber] = useState<number>(0)
-    useEffect(() => { setState(list) }, [list])
-    const boxScroll = useRef<HTMLDivElement>(null);
-    const { scrollYProgress, scrollY } = useScroll({
-        container: boxScroll,
-        offset: ["start start", "end end"],
-    });
+  const [scroll, dispatch] = useReducer(
+    (prev: keyable, next: keyable) => ({ ...prev, ...next }),
+    { scroll_number: 0, call: USER }
+  );
+  const [state, setState] = useState<Array<string | ReactNode>>();
+  useEffect(() => {
+    setState(list);
+  }, [list]);
+  const boxScroll = useRef<HTMLDivElement>(null);
+  const { scrollYProgress, scrollY } = useScroll({
+    container: boxScroll,
+    offset: ["start start", "end end"],
+  });
 
-    useEffect(() => {
-      scrollY.onChange((v) => {
-        if (scroll.call === "system") {
-
-        }
-        setScrollNumber((prev) => {
-          boxScroll.current?.scrollTo(0, scroll.scroll_number + 10);
-          return v;
-        });
+  useEffect(() => {
+    scrollY.onChange((v) => {
+      if (scroll.call === SYSTEM) {
+        dispatch({ scroll_number: scroll.scroll_number + 10, call: SYSTEM });
+        boxScroll.current?.scrollTo(0, scroll.scroll_number + 10);
         if (
           Number(((boxScroll.current?.offsetHeight ?? 0) / v).toFixed(1)) <= 0.8
         ) {
@@ -40,19 +44,20 @@ function InfinityScrollCarousel({ list }: Carousel) {
             return clone;
           });
         }
-      });
-    }, [scrollY]);
+      } else {
+      }
+    });
+  }, [scrollY]);
 
-    return (
-      <Box ref={boxScroll} h="100px" overflowY="scroll" w="fit-content">
-        <List spacing={3}>
-          {state?.map((i: string | ReactNode, index: number) => (
-            <ListItem key={"scroll" + _.uniqueId()}>{i}</ListItem>
-          ))}
-        </List>
-      </Box>
-    );
+  return (
+    <Box ref={boxScroll} h="100px" overflowY="scroll" w="fit-content">
+      <List spacing={3}>
+        {state?.map((i: string | ReactNode, index: number) => (
+          <ListItem key={"scroll" + _.uniqueId()}>{i}</ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 }
 
 export default InfinityScrollCarousel;
-
