a6a5f4fe73e269027b05b8048f3f197e65de36f1
yoarajota <joaovbscontato@gmail.com>
2023-03-08 19:02:39 -0300

    changes

--- src/components/infinitySlideCarousel.tsx
+++ src/components/infinitySlideCarousel.tsx
@@ -1,13 +1,13 @@
 import { Box, List, ListItem } from "@chakra-ui/react";
 import { Carousel, keyable } from "asset/types";
-import { animate, AnimatePresence, useAnimationControls, useScroll } from "framer-motion";
+import {
+  animate,
+  AnimatePresence,
+  useAnimationControls,
+  useScroll,
+} from "framer-motion";
 import _ from "lodash";
-import React, {
-    ReactNode,
-    useEffect,
-    useRef,
-    useState,
-} from "react";
+import React, { ReactNode, useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { wrap } from "popmotion";
 import { dir } from "console";
@@ -16,89 +16,104 @@ import { Colors } from "asset/enums";
 import NormalText from "./typography/normaltext";
 
 const variants = {
-    enter: (direction: number) => {
-        return {
-            y: direction > 0 ? 70 : -70,
-            opacity: 0
-        };
-    },
-    center: {
-        zIndex: 1,
-        y: 0,
-        opacity: 1
-    },
-    exit: (direction: number) => {
-        return {
-            zIndex: 0,
-            y: direction < 0 ? -70 : 70,
-            opacity: 0
-        };
-    }
+  center: {
+    zIndex: 1,
+    y: 0,
+    opacity: 1,
+  },
+  exit: (direction: number) => {
+    return {
+      y: direction < 0 ? 25 : -25,
+      scale: 1,
+    };
+  },
+  exit2: (direction: number) => {
+    return {
+      y: direction < 0 ? 25 : -25,
+      scale: 0.9,
+    };
+  },
+};
+
+const TRANSITION = {
+  duration: 0.1,
+  y: { type: "spring", stiffness: 300, damping: 30 },
+  opacity: { duration: 0.2 },
 };
 
 const swipeConfidenceThreshold = 30;
 const swipePower = (offset: number, velocity: number) => {
-    return Math.abs(offset) * velocity;
+  return Math.abs(offset) * velocity;
 };
 
 function InfinitySlideCarousel({ list }: Carousel) {
-    const [[page, direction], setPage] = useState([0, 0]);
-    const imageIndex = wrap(0, list?.length ?? 0, page);
+  const [[page, direction], setPage] = useState([0, 0]);
+  const imageIndex = wrap(0, list?.length ?? 0, page);
 
-    const paginate = (newDirection: number) => {
-        setPage([page + newDirection, newDirection]);
-    };
+  const paginate = (newDirection: number) => {
+    setPage([page + newDirection, newDirection]);
+  };
 
-    return (
-        <Box textAlign='center' display='flex' flexDirection='column' alignItems='center'>
-            <Box color={Colors.Orange} onClick={() => { paginate(1) }}>
-                <BiUpArrow />
+  return (
+    <Box
+      textAlign="center"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+    >
+      <Box
+        color={Colors.Orange}
+        onClick={() => {
+          paginate(1);
+        }}
+      >
+        <BiUpArrow />
+      </Box>
+      <motion.div
+        style={{ height: "6em", overflow: "hidden", display: "flex", justifyItems: 'center'}}
+      >
+        <AnimatePresence exitBeforeEnter>
+          <motion.div key={page}>
+            <Box
+              height="fit-content"
+              color="white"
+              display='flex'
+            >
+              {[-2, -1, 0, 1, 2].map((index) => (
+                <motion.div
+                  key={index}
+                  transition={TRANSITION}
+                  custom={direction}
+                  variants={variants}
+                  animate="center"
+                  exit={index === 0 ? "exit2" : "exit"}
+                  className="box-carousel"
+                  style={index === 0 ? {} : { scale: 0.9 }}
+                >
+                  <NormalText>
+                    {list &&
+                      (list[imageIndex + index]
+                        ? list[imageIndex + index]
+                        : list[
+                            (list.length + index + imageIndex) % list.length
+                          ])}
+                  </NormalText>
+                </motion.div>
+              ))}
             </Box>
-            <motion.div style={{ height: 'fit-content', overflow: "hidden" }}>
-                <AnimatePresence exitBeforeEnter >
-                    <motion.div
-                        key={page}
-                        custom={direction}
-                        variants={variants}
-                        initial="enter"
-                        animate="center"
-                        exit="exit"
-                        transition={{
-                            duration: 0.1,
-                            y: {type: "spring", stiffness: 300, damping: 30 },
-                            opacity: { duration: 0.2 }
-                        }}
-                        drag="y"
-                        dragConstraints={{ top: 0, bottom: 0 }}
-                        dragElastic={1}
-                        onDragEnd={(e, { offset, velocity }) => {
-                            const swipe = swipePower(offset.y, velocity.y);
-
-                            if (swipe < -swipeConfidenceThreshold) {
-                                paginate(1);
-                            } else if (swipe > swipeConfidenceThreshold) {
-                                paginate(-1);
-                            }
-                        }}
-
-                    >
-                        <Box
-                            alignItems="center"
-                            display='flex'
-                            height='fit-content'
-                            color="white"
-                        ><NormalText>
-                                {list && list[imageIndex]}
-                            </NormalText>
-                        </Box>
-                    </motion.div>
-                </AnimatePresence>
-            </motion.div>
-            <Box color={Colors.Orange} onClick={() => { paginate(-1) }}>
-                <BiDownArrow />
-            </Box>
-        </Box>
-    );
+          </motion.div>
+        </AnimatePresence>
+      </motion.div>
+      <Box
+        color={Colors.Orange}
+        onClick={() => {
+          paginate(-1);
+        }}
+      >
+        <BiDownArrow />
+      </Box>
+    </Box>
+  );
 }
 
 export default InfinitySlideCarousel;
