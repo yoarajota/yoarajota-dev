9a1d95f300c0a57c32e68411d9303337d750be26
yoarajota <joaovbscontato@gmail.com>
2023-06-11 14:44:22 -0300

    edit arquivo

--- src/components/animations/rainbowanimation.tsx
+++ src/components/animations/rainbowanimation.tsx
@@ -3,23 +3,23 @@ import { RainbowAnimation as RainbowAnimationType } from "../../asset/types";
 import { Colors } from "asset/enums";
 
 function RainbowAnimation({ otherColor, children }: RainbowAnimationType) {
-  return (
-    <motion.div
-      animate={{
-        color: [
-          otherColor ?? Colors.Orange,
-          Colors.DarkPurple,
-          otherColor ?? Colors.Orange,
-        ],
-      }}
-      transition={{
-        repeat: Infinity,
-        duration: 1,
-      }}
-    >
-      {children}
-    </motion.div>
-  );
+    return (
+        <motion.div
+            animate={{
+                color: [
+                    otherColor ?? Colors.Orange,
+                    Colors.DarkPurple,
+                    otherColor ?? Colors.Orange,
+                ],
+            }}
+            transition={{
+                repeat: Infinity,
+                duration: 1,
+            }}
+        >
+            {children}
+        </motion.div>
+    );
 }
 
 export default RainbowAnimation;
