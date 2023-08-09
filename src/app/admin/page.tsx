2958ea669be6a9f08af7c8637ad69a82da47cb5e
yoarajota <joaovbscontato@gmail.com>
2023-08-09 00:00:00 -0300

    new branche - work-in-progress

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,6 @@
 import { createClient } from "@vercel/edge-config";
 import Admin from "sections/admin";
+import "../../../statics/style.css";
 
 export default async function Page() {
   const json = await createClient(process.env.EDGE_CONFIG).getAll();
