851b128801ce5566da465908808198a60ec0b9db
yoarajota <joaovbscontato@gmail.com>
2023-09-11 21:03:37 -0300

    Others elements

--- src/components/admin/MongoDBData.tsx
+++ src/components/admin/MongoDBData.tsx
@@ -25,6 +25,7 @@ const MongoDBDATA = ({ data }: { data: Array<Info> }) => {
             locale={localeEn}
             onBlur={(val:any) => {
               // Handle onBlur logic here
+              console.log(val)
             }}
             placeholder={value.data}
             id={_.uniqueId("json-input-id-" + value.name)}
--- statics/initialElementsExtra.ts
+++ statics/initialElementsExtra.ts
@@ -3,6 +3,9 @@ import TypescriptSvg from "../src/components/svg/typescriptSvg";
 import PythonSvg from "../src/components/svg/pythonSvg";
 import PostgresSvg from "../src/components/svg/postgresSvg";
 import NodejsSvg from "../src/components/svg/nodejsSvg";
+import MongodbSvg from "../src/components/svg/mongodbSvg";
+import PhpSvg from "../src/components/svg/phpSvg";
+import JavascriptSvg from "../src/components/svg/javascriptSvg";
 
 const InitialElementsExtra: Node<any>[] = [
     {
@@ -80,6 +83,63 @@ const InitialElementsExtra: Node<any>[] = [
             Img: NodejsSvg,
         },
         position: { x: 240 * 4, y: 2850 },
+    },
+    {
+        id: "e-node-extra-head-mongodb",
+        type: "NodeHead",
+        sourcePosition: Position.Bottom,
+        selectable: false,
+        style: {
+            padding: "0",
+            minWidth: "260px",
+            background: "none",
+            border: "none",
+        },
+        data: {
+            label: "Mongodb",
+            xp: "1",
+            lvl: "2",
+            Img: MongodbSvg,
+        },
+        position: { x: 240 * 5, y: 2850 },
+    },
+    {
+        id: "e-node-extra-head-php",
+        type: "NodeHead",
+        sourcePosition: Position.Bottom,
+        selectable: false,
+        style: {
+            padding: "0",
+            minWidth: "260px",
+            background: "none",
+            border: "none",
+        },
+        data: {
+            label: "PHP",
+            xp: "1",
+            lvl: "2",
+            Img: PhpSvg,
+        },
+        position: { x: 240 * 6, y: 2850 },
+    },
+    {
+        id: "e-node-extra-head-javascript",
+        type: "NodeHead",
+        sourcePosition: Position.Bottom,
+        selectable: false,
+        style: {
+            padding: "0",
+            minWidth: "260px",
+            background: "none",
+            border: "none",
+        },
+        data: {
+            label: "Javascript",
+            xp: "1",
+            lvl: "2",
+            Img: JavascriptSvg,
+        },
+        position: { x: 240 * 7, y: 2850 },
     },
 ];
 
